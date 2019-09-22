ce71c624e51cd4af1b4edb8d3dc9c896b7d2c7ec
sheikheddy <sheikheddy@gmail.com>
2019-09-22 15:20:35 +0400

    Initial Commit

---
+++ src/coe420lab3/COE420Lab3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420lab3;
+
+/**
+ *
+ * @author b00075108
+ */
+public class COE420Lab3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
